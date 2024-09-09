65fd717e1b7a609f96e7c0bd0a0aba14be31ecb8
Santos2175 <santoshgurung949@gmail.com>
2024-09-09 19:24:11 +0545

    navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,41 @@
-import './App.css';
+import Nav from './components/nav/Nav.jsx';
+import Hero from './components/sections/Hero.jsx';
+import PopularProducts from './components/sections/PopularProducts.jsx';
+import SuperQuality from './components/sections/SuperQuality.jsx';
+import Services from './components/sections/Services.jsx';
+import SpecialOffers from './components/sections/SpecialOffers.jsx';
+import CustomerReviews from './components/sections/CustomerReviews.jsx';
+import Subscribe from './components/sections/Subscribe.jsx';
+import Footer from './components/sections/Footer.jsx';
 
-function App() {
-  return (
-    <>
-      <h1 className='text-3xl font-bold tracking-tight'>Hello world</h1>
-    </>
-  );
-}
+const App = () => (
+  <main className='relative'>
+    <Nav />
+    <section className='xl:padding-1 wide:padding-r padding-b'>
+      <Hero />
+    </section>
+    <section className='padding'>
+      <PopularProducts />
+    </section>
+    <section className='padding'>
+      <SuperQuality />
+    </section>
+    <section className='padding-x py-10'>
+      <Services />
+    </section>
+    <section className='padding'>
+      <SpecialOffers />
+    </section>
+    <section className='bg-pale-blue padding'>
+      <CustomerReviews />
+    </section>
+    <section className='padding-x sm:py-32 py-16 w-full'>
+      <Subscribe />
+    </section>
+    <section className='bg-black padding-x padding-t pb-8'>
+      <Footer />
+    </section>
+  </main>
+);
 
 export default App;
