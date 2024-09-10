170025daba0f5b62d59639f92133fc0fd2c8ccd5
Santos2175 <santoshgurung949@gmail.com>
2024-09-10 17:29:53 +0545

    super quality section added

--- src/components/sections/SuperQuality.jsx
+++ src/components/sections/SuperQuality.jsx
@@ -1,7 +1,43 @@
 import React from 'react';
+import Button from '../smallComponents/Button';
+import { arrowRight } from '../../assets/icons';
+import { shoe8 } from '../../assets/images';
 
 const SuperQuality = () => {
-  return <div>SuperQuality</div>;
+  return (
+    <section
+      id='about-us'
+      className='flex justify-between items-center max-lg:flex-col w-full max-container gap-10'>
+      <div className='flex flex-1 flex-col'>
+        <h2 className='font-palanquin text-4xl capitalize font-bold lg:max-w-lg'>
+          We Provide You{' '}
+          <span className='mt-3 text-coral-red inline-block'>Super</span>{' '}
+          <span className='mt-3 text-coral-red inline-block'>Quality</span>{' '}
+          Shoes
+        </h2>
+        <p className='mt-4 lg:max-w-lg info-text'>
+          Ensuring premium conmfort and styling, our meticuliously crafted
+          footwear is designed to elevate your experience, providing you with
+          unmatatched quality, innovation and a touch of elegance.
+        </p>
+        <p className='mt-6 lg:max-w-lg info-text'>
+          Our dedication to detail and excellence ensures your satisfaction.
+        </p>
+        <div className='mt-11'>
+          <Button label='view details' />
+        </div>
+      </div>
+      <div className='flex-1 flex justify-center items-center'>
+        <img
+          src={shoe8}
+          alt='shoe8'
+          width={570}
+          height={522}
+          className='object-contain'
+        />
+      </div>
+    </section>
+  );
 };
 
 export default SuperQuality;
--- src/components/smallComponents/Button.jsx
+++ src/components/smallComponents/Button.jsx
@@ -4,11 +4,13 @@ const Button = ({ label, iconURL }) => {
   return (
     <button className='flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red rounded-full text-white border-coral-red'>
       {label}
-      <img
-        src={iconURL}
-        alt='arrow right icon'
-        className='rounded-full h-5 w-5 ml-2'
-      />
+      {iconURL && (
+        <img
+          src={iconURL}
+          alt='arrow right icon'
+          className='rounded-full h-5 w-5 ml-2'
+        />
+      )}
     </button>
   );
 };
