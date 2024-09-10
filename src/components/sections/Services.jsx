a722d5e086a46fbd840e49eb2951a18e26bd64b6
Santos2175 <santoshgurung949@gmail.com>
2024-09-10 17:48:01 +0545

    services section added

--- src/components/sections/Services.jsx
+++ src/components/sections/Services.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import { services } from '../../constants';
+import ServiceCard from '../smallComponents/ServiceCard';
 
 const Services = () => {
-  return <div>Services</div>;
+  return (
+    <section className='flex justify-center flex-wrap gap-9'>
+      {services.map((service) => (
+        <ServiceCard key={service.label} {...service} />
+      ))}
+    </section>
+  );
 };
 
 export default Services;
